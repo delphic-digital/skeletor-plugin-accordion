b2435b77a1ad10d884cf32ed5417f91d530941e0
Jeff <jmills@delphicdigital.com>
2016-10-03 12:17:10 -0400

    More

--- src/accordion.js
+++ src/accordion.js
@@ -3,6 +3,13 @@ import {skeletor, SkeletorPlugin } from 'skeletor';
 class Accordion extends SkeletorPlugin {
 	constructor(element, options){
 		super(element, options)
+		this.VERSION = '0.1.0';
+
+		this.defaults = {
+			optionOne: true,
+			optionTwo: false
+		}
+
 		console.log('created a new accordion')
 	}
 }
